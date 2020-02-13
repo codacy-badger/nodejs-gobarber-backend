6db4ca66f75511ce556ecfe543a46c760b539081
Raul Neto <raulneto90@gmail.com>
2020-02-13 15:18:17 -0300

    Avatar de usuário

---
+++ src/database/migrations/20200213180428-create-files.js
@@ -0,0 +1,30 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('files', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('files');
+  },
+};
