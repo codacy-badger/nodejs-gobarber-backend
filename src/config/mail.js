6bc7ed155fd352cf33dd0af455b3c97da1870633
Raul Neto <raulneto90@gmail.com>
2020-02-17 11:39:35 -0300

    Variáveis ambiente e finalizado API

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'ae854bccf529e8',
-    pass: '84374490efbf99',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <norepy@gobarber.com.br>',
